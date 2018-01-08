a8fbbf540c3258e0d5b4b10ff79411bd6b84b33f
Alexy <alexandra.fina@yahoo.com>
2018-01-08 17:18:45 +0000

    Checking for maximum balance

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,12 +1,14 @@
 class Oystercard
   attr_reader :balance
   DEFAULT_CONSTANT = 0
+  LIMIT = 90
 
   def initialize(balance = DEFAULT_CONSTANT)
     @balance = balance
   end
 
   def top_up(value)
+    raise "Balance limit is #{LIMIT}" if @balance + value >= LIMIT
     @balance += value
   end
 
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,7 +2,7 @@ require './lib/oystercard.rb'
 
 describe Oystercard do
   it 'responds to top_up method' do
-    expect(subject).to respond_to(:top_up).with(1).argument  
+    expect(subject).to respond_to(:top_up).with(1).argument
   end
 
   it 'checks oystercard balance' do
@@ -10,8 +10,10 @@ describe Oystercard do
   end
 
   it 'tops up oystercard' do
-    oyster = Oystercard.new
-    oyster.top_up(5)
-    expect(oyster.balance).to eq(5)
+    expect { subject.top_up 5 }.to change{ subject.balance }.by 5
+  end
+
+  it 'raise an error if balance limit is exceeded' do
+    expect { subject.top_up(90) }.to raise_error("Balance limit is #{Oystercard::LIMIT}")
   end
 end
